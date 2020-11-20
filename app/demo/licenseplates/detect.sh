6be86ce4683d5394a18d9a246cb8f4af816618ed
lidongai <lidongai@hzcsai.com>
2020-11-20 22:17:54 +0800

    save

---
+++ app/demo/licenseplates/detect.sh
@@ -0,0 +1,1 @@
+python3 detect.py --source /raceai/data/datasets/licenseplates/images/train/psyen.jpg --weights runs/train/exp2/weights/best.pt --conf 0.1 --project tmp --name test --device cpu
---
+++ app/demo/licenseplates/train.sh
@@ -0,0 +1,1 @@
+python train.py --img 640 --batch 8 --epochs 30 --data licenseplates.yaml --cfg yolov5s.yaml --weights '/raceai/data/ckpts/yolov5/yolov5s.pt' --device 0
