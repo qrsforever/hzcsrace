0482382d62e083ddd63a84092057562f1c104056
lidongai <lidongai@hzcsai.com>
2021-04-25 16:14:38 +0800

    sae

--- app/demo/alphapose/train_halpe136.sh
+++ app/demo/alphapose/train_halpe136.sh
@@ -1,6 +1,17 @@
 set -x
 
-python ./train.py \
-    --snapshot 10 \
-    --work_dir /raceai/data/tmp/alphapose/output/halpe \
-    --cfg halpe_136/256x192_res50_lr1e-3_2x-regression.yaml
+master_addr=10.255.0.58
+master_port=8555
+nnodes=5
+
+python3 -m torch.distributed.launch \
+        --nproc_per_node=4 --nnodes=$nnodes --node=$1 \
+        --master_addr $master_addr --master_port $master_port \
+        train.py --snapshot 3 \
+	--work_dir /raceai/data/tmp/alphapose/output/halpe \
+	--cfg halpe_136/256x192_res50_lr1e-3_2x-regression.yaml
+
+# python ./train.py \
+#     --snapshot 10 \
+#     --work_dir /raceai/data/tmp/alphapose/output/halpe \
+#     --cfg halpe_136/256x192_res50_lr1e-3_2x-regression.yaml
