da553e4993a222162e36236dce060e309541462f
lidongai <lidongai@hzcsai.com>
2021-04-26 10:54:02 +0800

    save

--- app/demo/alphapose/train_halpe136.sh
+++ app/demo/alphapose/train_halpe136.sh
@@ -4,6 +4,15 @@ master_addr=10.255.0.58
 master_port=8555
 nnodes=5
 
+pids=`ps -eo pid,args | grep "train.py" | grep -v grep | cut -c1-5`
+
+echo $pids
+
+if [[ x$pids != x ]]
+then
+    kill -9 $pids
+fi
+
 python3 -m torch.distributed.launch \
         --nproc_per_node=1 --nnodes=$nnodes --node=$1 \
         --master_addr $master_addr --master_port $master_port \
