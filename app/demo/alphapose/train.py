d5822430b8491993368f7b7093ee877484f91ebc
lidongai <lidongai@hzcsai.com>
2021-04-26 14:56:18 +0800

    s

--- app/demo/alphapose/train.py
+++ app/demo/alphapose/train.py
@@ -36,7 +36,6 @@ def train(opt, train_loader, m, criterion, optimizer, writer):
     train_loader_tqdm = tqdm(train_loader, dynamic_ncols=True)
 
     for i, (inps, labels, label_masks, _, bboxes) in enumerate(train_loader_tqdm):
-        raise
         if isinstance(inps, list):
             inps = [inp.cuda().requires_grad_() for inp in inps]
         else:
