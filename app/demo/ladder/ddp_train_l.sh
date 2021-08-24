06527cdf00e07c8e5ad619aac0bdf765f90f9082
lidongai <lidongai@hzcsai.com>
2021-08-24 17:39:32 +0800

    save

--- app/demo/ladder/ddp_train_l.sh
+++ app/demo/ladder/ddp_train_l.sh
@@ -12,7 +12,7 @@ python3 -m torch.distributed.launch \
 	--nproc_per_node 1 --nnodes 3 --node_rank $DDP_RANK \
 	--master_addr $master_addr --master_port $master_port \
 	/raceai/codes/projects/yolov5/train.py \
-	--img-size 640 --batch-size 32 --epochs 320  --device 0 \
+	--img-size 640 --batch-size 60 --epochs 320  --device 0 \
 	--workers 4 --project $out --name ${pro}_l --exist-ok \
 	--data ./dataset.yaml \
 	--hyp ./hyperparameters.yaml \
