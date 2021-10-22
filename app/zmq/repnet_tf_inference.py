cfbbca3064147d4eb116eea7245cf0bb0b74abe5
Li Dong <705723886@qq.com>
2021-10-22 20:02:15 +0800

    save

--- app/zmq/repnet_tf_inference.py
+++ app/zmq/repnet_tf_inference.py
@@ -657,7 +657,6 @@ def inference(model, opt, resdata):
         race_object_put(osscli, outdir,
                 bucket_name=bucketname, prefix_map=prefix_map)
 
-    _video_save_progress(100)
     resdata['progress'] = 100.0
     resdata['sumcnt'] = sum_counts[-1]
     resdata['target_json'] = oss_domain + os.path.join(oss_path, os.path.basename(json_result_file))
